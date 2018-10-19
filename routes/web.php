9aacc7ff9e995821d1fe32fe78a269d867e7e612
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-19 16:46:21 +0800

    新增3個路由

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,15 @@
 
 Route::get('/', function () {
     return view('welcome');
+    return view('tasks');
+
 });
+
+Route::post('/task', function (Request $request) {
+    //
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
+
